c9feb93e5afb3cdb8268e42114451f99f71b3b0c
Israeli Perez Salinas <israeli.ps@gmail.com>
2021-01-22 17:24:13 -0600

    Lo estas haciendo muy bien

---
+++ 003-paquetes/gato/hola.go
@@ -0,0 +1,11 @@
+package gato
+
+import "fmt"
+
+func Hola() {
+	fmt.Println("Hola desde gato.")
+}
+
+func Come() {
+	fmt.Println("Los gatos comen pajaritos")
+}
---
+++ 003-paquetes/perro/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"gitlab.com/israeli/go.programming/003-paquetes/gato"
+)
+
+func main() {
+	fmt.Println("Hola desde Perro.")
+
+	gato.Hola()
+	gato.Come()
+}
